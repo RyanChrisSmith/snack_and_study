828104d6ec06bd5d5883b5c084134e951e2e9941
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-17 09:19:40 -0600

    all tests passing, until I build the third party api call

--- spec/requests/api/v1/learning_resources/specific_country_learning_resources_spec.rb
+++ spec/requests/api/v1/learning_resources/specific_country_learning_resources_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'Learning Resources API' do
     it 'returns an informational video title, link, and random images for the cournty specified' do
       get '/api/v1/learning_resources?country=laos'
 
-      require 'pry' ; binding.pry
     end
   end
 end
